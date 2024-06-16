fae01ec625f2aafe3e5fcd938d543d2262732555
haidaralfarisi <haidaralfarisi24@gmail.com>
2024-06-16 15:03:22 +0700

    set up template client

---
+++ resources/views/ClientView/Templates/scripts.blade.php
@@ -0,0 +1,6 @@
+<script src="{{ asset('TemplateClient/vendor/jquery/jquery.min.js') }}"></script>
+  <script src="{{ asset('TemplateClient/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+  <script src="{{ asset('TemplateClient/assets/js/isotope.min.js') }}"></script>
+  <script src="{{ asset('TemplateClient/assets/js/owl-carousel.js') }}"></script>
+  <script src="{{ asset('TemplateClient/assets/js/counter.js') }}"></script>
+  <script src="{{ asset('TemplateClient/assets/js/custom.js') }}"></script>
